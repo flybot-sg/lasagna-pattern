b7e318a55d8ac75f244742913b9848617693608b
chickendreanso <andreanpriadi@icloud.com>
2026-09-21 13:13:07 +0800

    fix(pattern): run clojure.test suite on the JVM via kaocha

---
+++ pattern/test/sg/flybot/rct_test.clj
@@ -0,0 +1,8 @@
+(ns sg.flybot.rct-test
+  "Wrapper to run RCT tests alongside clojure.test"
+  (:require
+   [clojure.test :refer [deftest]]
+   [com.mjdowney.rich-comment-tests.test-runner :as tr]))
+
+(deftest rct-tests
+  (tr/run-tests-in-file-tree! :dirs #{"src" "test"}))
